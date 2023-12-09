999d8e4f5f8fcbf1fc9816fe8e2cee13adacc3db
dung1233 <139690716+dung1233@users.noreply.github.com>
2023-12-09 10:02:47 +0700

    Update Banktest.java

--- PRACTICALEXAM/Banktest.java
+++ PRACTICALEXAM/Banktest.java
@@ -7,18 +7,20 @@ public class Banktest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        try {
-            System.out.print("Enter initial balance: ");
-            double initialBalance = scanner.nextDouble();
-            System.out.print("Enter annual interest rate: ");
-            double interestRate = scanner.nextDouble();
-            Bank myBank = new Bank(initialBalance, interestRate);
-            double monthlyInterest = myBank.calculateInterest();
-            System.out.printf("Monthly interest: $%.2f%n", monthlyInterest);
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please enter numeric values.");
-        } finally {
-            scanner.close();
-        }
+       
+        System.out.print("Enter initial balance: ");
+        double initialBalance = scanner.nextDouble();
+
+        System.out.print("Enter annual interest rate: ");
+        double interestRate = scanner.nextDouble();
+
+       
+        Bank myBank = new Bank(initialBalance, interestRate);
+
+        
+        double monthlyInterest = myBank.calculateInterest();
+        System.out.printf("Monthly interest: $%.2f%n", monthlyInterest);
+
+        scanner.close();
     }
 }
