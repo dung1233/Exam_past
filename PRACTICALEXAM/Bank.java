40aab8d5c64f44a4084de95d7ce5173f9e87bee5
dung1233 <139690716+dung1233@users.noreply.github.com>
2023-12-09 09:58:23 +0700

    Update Bank.java

--- PRACTICALEXAM/Bank.java
+++ PRACTICALEXAM/Bank.java
@@ -7,13 +7,13 @@ public class Bank {
     private double balance;
     private double annualInterestRate;
 
-    // Constructor to initialize balance and rate
+    
     public Bank(double initialBalance, double rate) {
         balance = initialBalance;
         annualInterestRate = rate;
     }
 
-    // Function to calculate monthly interest
+   
     public double calculateInterest() {
         return balance * (annualInterestRate / 1200);
     }
